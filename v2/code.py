2d7faf72ab269810dd62f2b327c2f28226b02a85
Javier Verdú <77059171+javiverdu@users.noreply.github.com>
2023-05-01 12:26:07 +0200

    Update code.py

--- v2/code.py
+++ v2/code.py
@@ -51,7 +51,7 @@ keensy.keymap = [
  [  
    KC.ESCAPE,        KC.F1,      KC.F2,    KC.F3,    KC.F4,    KC.F5,    KC.F6,      KC.F7,    KC.F8,    KC.F9,    KC.F10,     KC.F11,       KC.F12,         KC.PSCREEN, KC.HOME, 
    KC.GRAVE,         KC.N1,      KC.N2,    KC.N3,    KC.N4,    KC.N5,    KC.N6,      KC.N7,    KC.N8,    KC.N9,    KC.N0,      KC.MINUS,     KC.EQUAL,       KC.BSPACE,  KC.DELETE, 
-   KC.TAB,           KC.Q,       KC.W,     KC.E,     KC.R,     KC.T,     KC.Y,       KC.U,     KC.I,     KC.O,     KC.P,       KC.RBRACKET,  KC.LBRACKET,    KC.ENTER,   KC.PGUP,
+   KC.TAB,           KC.Q,       KC.W,     KC.E,     KC.R,     KC.T,     KC.Y,       KC.U,     KC.I,     KC.O,     KC.P,       KC.LBRACKET,  KC.RBRACKET,    KC.ENTER,   KC.PGUP,
    KC.CAPSLOCK,      KC.A,       KC.S,     KC.D,     KC.F,     KC.G,     KC.H,       KC.J,     KC.K,     KC.L,     KC.SCOLON,  KC.QUOTE,     KC.NONUS_HASH,  KC.NO,      KC.PGDOWN,
    KC.LSHIFT,KC.NONUS_BSLASH,    KC.Z,     KC.X,     KC.C,     KC.V,     KC.B,       KC.N,     KC.M,     KC.COMMA, KC.DOT,     KC.SLASH,     KC.RSHIFT,      KC.UP,      KC.END,
    KC.LCTRL,         KC.LGUI,    KC.LALT,  KC.NO,    KC.NO,    KC.NO,    KC.SPACE,   KC.NO,    KC.NO,    KC.RALT,  KC.RGUI,    KC.RCTRL,     KC.LEFT,        KC.DOWN,    KC.RIGHT,           
